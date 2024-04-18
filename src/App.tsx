d6a7c8010d0a7dacbc3ea849959cc1f3e7dddf49
Aidan Matrician <aidanmatrician@gmail.com>
2024-04-18 14:31:53 -0400

    fix

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { HashRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
+import Ditto from "./Ditto/index.tsx";
+
+function App() {
+  return (
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/Ditto" />} />
+          <Route path="/Ditto/*" element={<Ditto />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
--- src/Ditto/index.tsx
+++ src/Ditto/index.tsx
@@ -2,17 +2,16 @@ import React, { useState } from "react";
 import DittoNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Profile from "./Profile";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Ditto() {
-  
   return (
     // <Provider store={store}>
     <div className="d-flex">
       {/* <DittoNavigation /> */}
       <div style={{ flexGrow: 1 }}>
-      <Routes>
-      <Route path="/" element={<Navigate to="Home" />} />
+        <Routes>
+          <Route path="/" element={<Navigate to="Home" />} />
         </Routes>
       </div>
     </div>
