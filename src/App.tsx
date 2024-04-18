c122ab42747c1f40daeab7f92303ef1abb87f412
Daniel Kim <51827691+folaroma@users.noreply.github.com>
2024-04-18 17:34:41 -0400

    refactor + add chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { HashRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router";
+import { ChakraProvider } from '@chakra-ui/react'
 import Ditto from "./Ditto/index.tsx";
 
 function App() {
   return (
-    <HashRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Navigate to="/Ditto" />} />
-          <Route path="/Ditto/*" element={<Ditto />} />
-        </Routes>
-      </div>
-    </HashRouter>
+    <ChakraProvider>
+      <HashRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/Ditto" />} />
+            <Route path="/Ditto/*" element={<Ditto />} />
+          </Routes>
+      </HashRouter>
+    </ChakraProvider>
   );
 }
 
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,8 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      Home
-      <div>Homepage Contents</div>
+
     </div>
   );
 }
