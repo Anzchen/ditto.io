8a6ef4d158467751d66226f99ebc405ba3247364
Aidan Matrician <aidanmatrician@gmail.com>
2024-04-21 04:00:13 -0400

    displaying top 3 songs

--- src/components/Home/Message/index.jsx
+++ src/components/Home/Message/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Text, Box } from "@chakra-ui/react"
+import { Text, Box } from "@chakra-ui/react";
 
 function Message(user) {
   return (
     <Box ml="2em" as="b" color="white" w="14em">
-      <Text>Hello, {user.first_name}!<br/></Text>
-      <Text><br/>
-        Here are your Top 4 Songs!
-        Click on any of the songs below to write a Review.
+      <Text>
+        Hello, {user.first_name}!<br />
+      </Text>
+      <Text>
+        <br />
+        Here are your Top 3 Songs! Click on any of the songs below to write a
+        Review.
       </Text>
     </Box>
-    );
-  }
+  );
+}
 export default Message;
--- src/components/Song/index.jsx
+++ src/components/Song/index.jsx
@@ -1,6 +1,16 @@
-function Song() {
+import React, { useState } from "react";
+import { Text, Box } from "@chakra-ui/react";
+
+function Song(props) {
+  const [rating, setRating] = useState(0);
+
   return (
-      console.log("Song")
-    );
-  }
+    <Box ml="2em" as="b" bg="white" w="100%" borderRadius=".5em">
+      <Text color="black">
+        {props.song.name}
+        <br />
+      </Text>
+    </Box>
+  );
+}
 export default Song;
