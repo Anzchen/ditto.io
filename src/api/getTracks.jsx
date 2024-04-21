6c1ba7a4a524a26f9f5465b8e172b7453de1aaca
Aidan Matrician <aidanmatrician@gmail.com>
2024-04-21 00:13:21 -0400

    initial changes to include spotify api work on the home page

--- src/api/getTracks.jsx
+++ src/api/getTracks.jsx
@@ -1,27 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useTracks = (accessToken) => {
-    const [tracks, setTracks] = useState([]);
+  const [tracks, setTracks] = useState([]);
 
-    useEffect(() => {
-        if (accessToken) {
-            fetch('https://api.spotify.com/v1/tracks?ids=7ouMYWpwJ422jRcDASZB7P,4VqPOruhp5EdPBeR92t6lQ,2takcwOaAZWiXQijPHIx7B', {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer ' + accessToken
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                setTracks(data.tracks);
-            })
-            .catch(error => {
-                console.error('Error fetching tracks:', error);
-            });
+  useEffect(() => {
+    if (accessToken) {
+      fetch(
+        "https://api.spotify.com/v1/tracks?ids=7ouMYWpwJ422jRcDASZB7P,4VqPOruhp5EdPBeR92t6lQ,2takcwOaAZWiXQijPHIx7B",
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + accessToken,
+          },
         }
-    }, [accessToken]);
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setTracks(data.tracks);
+        })
+        .catch((error) => {
+          console.error("Error fetching tracks:", error);
+        });
+    }
+  }, [accessToken]);
 
-    return tracks;
+  return tracks;
 };
 
 export default useTracks;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,42 +1,49 @@
-import React, { useState } from "react";
-import { VStack, HStack } from "@chakra-ui/react"
+import React, { useEffect, useState } from "react";
+import { VStack, HStack } from "@chakra-ui/react";
 import Discover from "./Discover/";
 import Message from "./Message/";
 import Song from "../Song/";
 import SpotifyAPITesting, { API_URL } from "../../consts";
+import useTracks from "../../api/getTracks";
+import useAccessToken from "../../api/getAccessToken";
 
 function Home() {
-  
   const [user, setUser] = useState({});
-  const getProfile = () => {
-    fetch(`${API_URL}/profile`, {
-      method: 'POST',
-      credentials: 'include'
-    }).then(res => res.json())
-    .then(user => {
-      setUser(user);
-    });
-  }
-  
   const [topSongs, setTopSongs] = useState([]);
+  useEffect(() => {
+    const getProfile = async () => {
+      try {
+        const res = await fetch(`http://localhost:4000/api/users/profile`, {
+          method: "POST",
+          credentials: "include",
+        });
+        const userData = await res.json();
+        setUser(userData);
+        setTopSongs(userData.songs || []);
+      } catch (error) {
+        console.error("Error fetching user profile:", error);
+      }
+    };
+
+    getProfile();
+  }, []);
+  const accessToken = useAccessToken();
+
+  const tracks = useTracks(accessToken);
+  console.log(tracks);
+
   return (
     <VStack>
       <HStack mt="7em" mr="45em">
-        <Discover/>
-        { user &&
-          <Message user={user}/>
-        }
+        <Discover />
+        {user && <Message user={user} />}
       </HStack>
       <HStack>
-        {
-          topSongs.map(song => {
-            return (
-              <Song key={song.song_id} song={song}/>
-            );
-          })
-        }
+        {tracks.map((song) => {
+          return <Song key={song.song_id} song={song} />;
+        })}
       </HStack>
-      <SpotifyAPITesting/>
+      {/* <SpotifyAPITesting /> */}
     </VStack>
   );
 }
