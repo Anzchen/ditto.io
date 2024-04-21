fbd4a239ba3dc34e9949057ca353c3ae6b3bf4dc
christinaw20 <68133503+christinaw20@users.noreply.github.com>
2024-04-21 12:44:36 -0400

    made homepage songs look like figma mockup and added redirects to /review/{.song.id}

--- src/components/Song/index.jsx
+++ src/components/Song/index.jsx
@@ -1,18 +1,53 @@
-import React, { useState } from "react";
-import { Text, Box } from "@chakra-ui/react";
+import React from "react";
+import { Text, Box, Flex, Image } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 function Song(props) {
-  const [rating, setRating] = useState(0);
+  const navigate = useNavigate();
+
+  const handleSongClick = () => {
+    // Redirect to the review page when song box is clicked
+    navigate(`/reviews/${props.song.id}`);
+  };
 
   return (
-    <Box ml="2em" as="b" bg="white" w="100%" borderRadius=".5em">
-      <Text color="black">
+    <Flex
+      direction="column"
+      align="center"
+      ml="4em"
+      bg="white"
+      w="12em"
+      borderRadius=".5em"
+      p="1em"
+      boxShadow="md"
+      cursor="pointer" // Add cursor pointer for clickable effect
+      onClick={handleSongClick} 
+    >
+      {/* Album Cover */}
+      <Image
+        src={props.song.album.images[0].url} 
+        alt={props.song.album.name} 
+        borderRadius=".5em"
+        mb="0.5em"
+      />
+
+      {/* Song Title */}
+      <Text fontWeight="bold" color="black" fontSize="lg" > 
         {props.song.name}
-        <br />
       </Text>
-      {/* here's a link to all the things we can get from a song:
-      https://developer.spotify.com/documentation/web-api/reference/get-several-tracks  */}
-    </Box>
+
+      {/* Artist Name */}
+      <Text fontSize="sm" color="gray.600">
+        {props.song.artists.map((artist) => artist.name).join(", ")}
+      </Text>
+    </Flex>
   );
 }
+
 export default Song;
+
+
+
+{/* here's a link to all the things we can get from a song:
+      https://developer.spotify.com/documentation/web-api/reference/get-several-tracks  */}
+
