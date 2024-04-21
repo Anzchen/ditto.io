f17285c9ce5dbeaca54d38bb0572135129361798
Aidan Matrician <aidanmatrician@gmail.com>
2024-04-21 04:53:52 -0400

    added conditional rendering based on if user is logged in or not

--- src/components/Home/Message/index.jsx
+++ src/components/Home/Message/index.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, Box } from "@chakra-ui/react";
 
-function Message(user) {
+function Message(props) {
+  useEffect(() => {
+    // This effect will run whenever the loggedIn or user prop changes
+    // You can perform any necessary actions or state updates here
+    console.log("Props changed:", props.isLoggedIn, props.user);
+  }, [props.isLoggedIn, props.user]);
+
   return (
-    <Box ml="2em" as="b" color="white" w="14em">
-      <Text>
-        Hello, {user.first_name}!<br />
-      </Text>
-      <Text>
-        <br />
-        Here are your Top 3 Songs! Click on any of the songs below to write a
-        Review.
-      </Text>
-    </Box>
+    <>
+      {props.isLoggedIn ? (
+        <Box ml="2em" as="b" color="white" w="14em">
+          <Text>
+            Hello, {props.user.username}!<br />
+          </Text>
+          <Text>
+            <br />
+            Here are your Top 3 Songs! Click on any of the songs below to write
+            a Review.
+          </Text>
+        </Box>
+      ) : (
+        <Box ml="2em" as="b" color="white" w="14em">
+          <Text color="white">Here are the Top 3 Songs on Spotify today!</Text>
+        </Box>
+      )}
+    </>
   );
 }
+
 export default Message;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { VStack, HStack } from "@chakra-ui/react";
+import { VStack, HStack, Text } from "@chakra-ui/react";
 import Discover from "./Discover/";
 import Message from "./Message/";
 import Song from "../Song/";
 import SpotifyAPITesting, { API_URL } from "../../consts";
 import useTracks from "../../api/getTracks";
 import useAccessToken from "../../api/getAccessToken";
-
+import { useInstantTransition } from "framer-motion";
+import axios from "axios";
 function Home() {
   const [user, setUser] = useState({});
   const [topSongs, setTopSongs] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     const getProfile = async () => {
       try {
@@ -20,11 +22,16 @@ function Home() {
         const userData = await res.json();
         setUser(userData);
         setTopSongs(userData.songs || []);
+        // const loggedIn = await axios.get(
+        //   "http://localhost:4000/api/users/isLoggedIn"
+        // );
+        // setIsLoggedIn(loggedIn.data);
+        setIsLoggedIn(true);
       } catch (error) {
+        setIsLoggedIn(false);
         console.error("Error fetching user profile:", error);
       }
     };
-
     getProfile();
   }, []);
   const accessToken = useAccessToken();
@@ -36,13 +43,25 @@ function Home() {
     <VStack>
       <HStack mt="7em" mr="45em">
         <Discover />
-        {user && <Message user={user} />}
+        {user && <Message user={user} isLoggedIn={isLoggedIn} />}
+        {/* user && <Message user={user} loggedIn={isLoggedIn} /> */}
       </HStack>
-      <HStack>
-        {tracks.map((song) => {
-          return <Song key={song.song_id} song={song} />;
-        })}
-      </HStack>
+      {isLoggedIn ? (
+        <HStack>
+          {tracks.length > 0 ? (
+            tracks.map((song) => {
+              return <Song key={song.song_id} song={song} />;
+            })
+          ) : (
+            <Text color="white">No songs available.</Text>
+          )}
+        </HStack>
+      ) : (
+        <HStack>
+          <Text color="white">Log in to see user data </Text>
+        </HStack>
+      )}
+
       {/* <SpotifyAPITesting /> */}
     </VStack>
   );
